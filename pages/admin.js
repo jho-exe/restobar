765a43685266ec76eba35ff35a095a5ea8573a69
Javier Hormazabal <javier.hormazabal.182@gmail.com>
2023-07-07 10:20:23 -0400

    codigo funcional

--- pages/admin.js
+++ pages/admin.js
@@ -2,10 +2,12 @@ import useSWR from 'swr'
 import axios from 'axios'
 import AdminLayout from "../layout/AdminLayout";
 import Orden from '../components/Orden'
+import React, { useState } from 'react';
 
 export default function Admin() {
     const fetcher = () => axios('/api/ordenes').then(datos => datos.data) 
     const { data, error, isLoading} = useSWR('/api/ordenes', fetcher, {refreshInterval: 100})
+    
 
     return (
         <AdminLayout pagina={'Admin'}>
